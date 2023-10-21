d1ac572fa76b029faa869c6d96062b57ff9414b8
sanraith <sanraith@users.noreply.github.com>
2023-10-21 23:12:59 +0000

    Added basic CLI.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,40 @@
-import scala.math._
+@main
+def main(args: String*) = {
+  clearConsole
+  println("Advent of Code 2023")
+  println("━━━━━━━━━━━━━━━━━━━")
 
-object Main extends App {
-  (0 to 100)
-    .map(BigInt(2).pow(_))
-    .map(x => {
-      println(s"Hello ${x}")
-    })
+  val dayRegex = """(\d+)""".r
+  val firstArg = args.headOption.map(_.toLowerCase)
+  firstArg match {
+    case Some("all") | None         => println("Solving all days")
+    case Some("last")               => println("Solving last available day...")
+    case Some("scaffold")           => scaffold(getDays(args.drop(1)))
+    case Some("day") | Some("days") => solveDays(getDays(args.drop(1)))
+    case Some(dayRegex(day))        => solveDays(getDays(args))
+    case Some(_) =>
+      println(s"Unknown parameter sequence: ${args.mkString(", ")}")
+  }
+
+  println
+}
+
+def clearConsole = print("\u001b[2J")
+
+def getDays(seq: Seq[String]): Seq[Int] = seq.map(_.toIntOption).flatMap(x => x)
+
+def plural(seq: Seq[Any]): String = if (seq.length > 1) "s" else ""
+
+def scaffold(days: Seq[Int]) = {
+  if (days.length > 0) {
+    println(s"Scaffolding day${plural(days)} ${days.mkString(", ")}...")
+  } else {
+    println(s"Scaffolding missing days...")
+  }
+}
+
+def solveDays(days: Seq[Int]) = {
+  println(
+    s"Solving day${plural(days)} ${days.mkString(", ")}..."
+  )
 }
