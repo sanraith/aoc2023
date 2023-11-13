d2a9092fc98157109177b17c1587dc6b10d41578
Soma Zsják <zsjaks@gmail.com>
2023-11-14 00:53:22 +0100

    Add progress to console runner.

--- README.md
+++ README.md
@@ -16,12 +16,12 @@ Solutions for Advent of Code 2023 in Scala.
 - [x] Test framework
   - [x] Use test data starting with line break
   - [x] Use real data
-- [ ] Run framework
+- [x] Run framework
   - [x] Run solutions
   - [x] Use real data
   - [x] Show time
   - [x] Show multiline results in new line
-  - [ ] Show progress
+  - [x] Show progress
 
 ## Commands
 
--- src/main/scala/hu/sanraith/aoc2023/cli/SolutionRunner.scala
+++ src/main/scala/hu/sanraith/aoc2023/cli/SolutionRunner.scala
@@ -1,26 +1,51 @@
 package hu.sanraith.aoc2023.cli
 
-import hu.sanraith.aoc2023.solution._
 import hu.sanraith.aoc2023.common._
+import hu.sanraith.aoc2023.solution._
 
-class ConsoleContext(val input: String) extends Context:
-  override def progress(value: Double): Unit = {}
+import java.util.concurrent.TimeUnit
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
 
 object SolutionRunner:
   def run(solution: Solution) =
     val input = Util.loadInputFromFile(solution)
     val context = ConsoleContext(input)
     try
-      timed(1, () => solution.part1(context))
-      timed(2, () => solution.part2(context))
+      runPart(1, solution, context)
+      runPart(2, solution, context)
     catch case e => println(e)
 
-  def timed[T](part: Int, work: () => String): Unit =
-    var start = System.nanoTime()
-    val result =
-      try work()
-      catch e => e.toString
-    val end = System.nanoTime()
+  private def runPart(part: Int, solution: Solution, context: ConsoleContext): Unit =
+    context.isCompleted = false
+    context.progress = None
+
+    val start = System.nanoTime()
+    val workFuture = Future:
+      val result =
+        try
+          part match
+            case 1 => solution.part1(context)
+            case _ => solution.part2(context)
+        catch e => e.toString
+      context.isCompleted = true
+      (result, System.nanoTime())
+
+    val sameLine = LineRewriter()
+    val progressFuture = Future:
+      while (!context.isCompleted)
+        val time = timeStr(start, System.nanoTime())
+        val percentage = context.progress.map(x => f" ${x * 100}%2.2f%%").getOrElse("")
+        sameLine.print(s"Part $part... ($time)$percentage")
+        Thread.sleep(75)
+
+    val (result, end) = Await.result(workFuture, Duration.Inf)
+    Try(Await.ready(progressFuture, Duration.create(5, TimeUnit.SECONDS)))
 
     val lineStart = s"Part $part (${timeStr(start, end)}): "
     val formattedResult = result match
@@ -28,6 +53,23 @@ object SolutionRunner:
         lines.split("\n").mkString("\n".padTo(lineStart.length + 1, ' '))
       case line => line
 
-    println(s"$lineStart$formattedResult")
+    sameLine.println(s"$lineStart$formattedResult")
+  end runPart
 
   def timeStr(start: Long, end: Long): String = s"${(end - start) / 1000000} ms"
+
+/** Prints content to the same console line until println is called. */
+class LineRewriter:
+  var prevLength: Int = 0
+
+  def print(line: String): Unit =
+    Predef.print(s"\r${line.padTo(prevLength, ' ')}")
+    prevLength = line.length
+
+  def println(line: String): Unit =
+    print(s"$line\n")
+
+class ConsoleContext(val input: String) extends Context:
+  var progress: Option[Double] = None
+  var isCompleted: Boolean = false
+  override def progress(value: Double): Unit = progress = Some(value)
