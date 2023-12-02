123730865237c5bc2e3aedaebeb9c2aca4f2d941
Soma Zsják <zsjaks@gmail.com>
2023-12-02 08:11:28 +0100

    Solved day 2.

---
+++ src/main/scala/hu/sanraith/aoc2023/solution/Day02.scala
@@ -0,0 +1,36 @@
+package hu.sanraith.aoc2023.solution
+
+/** Solution for https://adventofcode.com/2023/day/2 */
+class Day02 extends Solution:
+  override val title: String = "Cube Conundrum"
+
+  override def part1(ctx: Context): String =
+    val bag = Map("red" -> 12, "green" -> 13, "blue" -> 14)
+    parseGames(ctx.input)
+      .filter(_.hands.forall(_.forall((color, count) => count <= bag(color))))
+      .map(_.id)
+      .sum
+      .toString
+
+  override def part2(ctx: Context): String =
+    parseGames(ctx.input)
+      .map: game =>
+        game.hands
+          .fold(Map.empty)((acc, hand) =>
+            acc ++ hand.map((color, count) => (color, acc.getOrElse(color, 0).max(count)))
+          )
+          .map((_, count) => count)
+          .product
+      .sum
+      .toString
+
+  def parseGames(input: String) =
+    val handRegex = """(\d+) (\w+)""".r
+    input.linesIterator.zipWithIndex
+      .map: (line, i) =>
+        val hands = line
+          .split(";")
+          .map(p => handRegex.findAllMatchIn(p).map(m => (m.group(2), m.group(1).toInt)).toMap)
+        Game(i + 1, hands)
+
+case class Game(id: Int, hands: Seq[Map[String, Int]])
---
+++ src/test/scala/hu/sanraith/aoc2023/solution/Day02Test.scala
@@ -0,0 +1,40 @@
+package hu.sanraith.aoc2023.solution
+import hu.sanraith.aoc2023.SolutionTestSpec
+
+/** Tests for https://adventofcode.com/2023/day/2 */
+class Day02Test extends SolutionTestSpec:
+
+  describe("Day02 for example input") {
+    given day: Solution = Day02()
+
+    it("solves part 1"):
+      assertPart(
+        day.part1,
+        expected = "8",
+        input = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+      )
+
+    it("solves part 2"):
+      assertPart(
+        day.part2,
+        expected = "2286",
+        input = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+      )
+  }
+
+  describe("Day02 for puzzle input") {
+    given day: Solution = Day02()
+
+    it("solves part 1")(assertPart(day.part1, "2256"))
+    it("solves part 2")(assertPart(day.part2, "74229"))
+  }
