ce015992cd761185d6285fbc4a030c1b2b9e773e
Soma Zsják <zsjaks@gmail.com>
2023-12-03 11:00:45 +0100

    Fixed replace bug in TemplateFiller.

--- src/main/scala/hu/sanraith/aoc2023/cli/TemplateFiller.scala
+++ src/main/scala/hu/sanraith/aoc2023/cli/TemplateFiller.scala
@@ -1,12 +1,13 @@
 package hu.sanraith.aoc2023.cli
 
 import scala.util.matching.Regex
+import java.util.regex.Matcher
 
 class TemplateFiller(template: String):
 
   /** Replaces the placeholder with the given content. */
   def fill(placeholder: String, content: String): TemplateFiller =
-    TemplateFiller(template.replaceAll(placeholder, content))
+    TemplateFiller(template.replaceAll(placeholder, Matcher.quoteReplacement(content)))
 
   /** Replaces the placeholder with the given lines, keeping the placeholders original indentation
     * level for each line.
@@ -20,7 +21,7 @@ class TemplateFiller(template: String):
     withIndentRegex.findFirstMatchIn(template) match
       case Some(m) =>
         val indent = m.group(1)
-        val linesStr = indent + lines.mkString(s"$lineSeparator$indent")
+        val linesStr = Matcher.quoteReplacement(indent + lines.mkString(s"$lineSeparator$indent"))
         TemplateFiller(withIndentRegex.replaceFirstIn(template, linesStr))
       case None => this
 
