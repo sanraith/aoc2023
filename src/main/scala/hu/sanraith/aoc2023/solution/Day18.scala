c33bcdae281ee4fa88e4fb7b6deff1fa8bd28f84
Soma Zsják <sanraith@users.noreply.github.com>
2023-12-18 23:02:37 +0000

    Refactor day 18.

--- src/main/scala/hu/sanraith/aoc2023/solution/Day18.scala
+++ src/main/scala/hu/sanraith/aoc2023/solution/Day18.scala
@@ -5,8 +5,6 @@ import scala.collection.{mutable => mut}
 
 /** Solution for https://adventofcode.com/2023/day/18 */
 class Day18 extends Solution:
-  val enableDebugPrint = false
-
   override val title: String = "Lavaduct Lagoon"
 
   override def part1(ctx: Context): Long =
@@ -23,40 +21,21 @@ class Day18 extends Solution:
     calcArea(commands, ctx)
 
   def calcArea(commands: Seq[Command], ctx: Context) =
-    // Normalize coordinates
-    val (minPos, maxPos, _) = commands.foldLeft(Point(0, 0), Point(0, 0), Point(0, 0)):
-      case ((min, max, pos), c) =>
-        (
-          Point(math.min(pos.x, min.x), math.min(pos.y, min.y)),
-          Point(math.max(pos.x, max.x), math.max(pos.y, max.y)),
-          pos + c.direction * c.length
-        )
-    val start = minPos * -1
-    val bottomRight = maxPos - minPos
+    // Convert commands to points
+    val (points, _) = commands.foldLeft((Seq.empty[Point], Point(0, 0))):
+      case ((points, pos), c) =>
+        val nextPos = pos + c.direction * c.length
+        (points :+ nextPos, nextPos)
 
-    val tiles: mut.Map[Point, Long] = mut.Map.empty
-    var pos = start
-    val points = commands.map: c =>
-      pos += c.direction * c.length
-      pos
-
+    // add each coord's next neighbour too
     val dPoints = points.flatMap(p => Seq(p, p + Point(1, 1)))
-    var rectId = 0
+    // create rectangles of (topLeft, bottomRight) where each point has the same state
     val rects = (for
       xp <- dPoints.map(_.x).toSet.toSeq.sorted.sliding(2).toSeq
       yp <- dPoints.map(_.y).toSet.toSeq.sorted.sliding(2).toSeq
-    yield (xp, yp)).map:
-      case (Seq(x1, x2), Seq(y1, y2)) =>
-        if (enableDebugPrint)
-          (x1 until x2)
-            .flatMap(x => (y1 until y2).map(y => Point(x, y)))
-            .foreach(p => tiles.addOne(p, rectId))
-          rectId += 1
-          // debugPrint(tiles, bottomRight + Point(1, 1))
-        Point(x1, y1) -> Point(x2, y2)
-    // debugPrint(tiles, bottomRight + Point(1, 1))
+    yield (xp, yp)).map { case (Seq(x1, x2), Seq(y1, y2)) => Point(x1, y1) -> Point(x2, y2) }
 
-    val pointPairs = (Seq(points.last) ++ points.take(points.length - 1)).zip(points)
+    val pointPairs = (points.last +: points.take(points.length - 1)).zip(points)
     val horizontalEdges = pointPairs
       .filter((p1, p2) => p1.y == p2.y)
       .map((p1, p2) => (p1, p2, if (p1.x < p2.x) East else West))
@@ -67,55 +46,23 @@ class Day18 extends Solution:
       .sortBy(_._1.x)
 
     val rectCount = rects.length.toDouble
-    val area = rects.iterator
+    rects.iterator
       .tapEachWithIndex((_, i) => ctx.progress(i / rectCount))
-      .map((p1, p2) => {
-        // a. is on vertical edge
-        // b. is on horizontal edge
-        // c. has odd number of vertical edges on its left only counting (a until b) of the edge
-        val Point(x, y) = p1
-        val isIncluded =
-          horizontalEdges.exists((a, b, d) => y == a.y && (a.x to b.x by d.x).contains(x)) ||
-            verticalEdges.exists((a, b, d) => x == a.x && (a.y to b.y by d.y).contains(y)) || {
-              val leftEdgeDirs = verticalEdges
-                .takeWhile((a, _, _) => a.x < x)
-                .filter((a, b, d) => (a.y to b.y by d.y).contains(y))
-                .map((_, _, d) => d)
-              (leftEdgeDirs.length > 0 && leftEdgeDirs.head == leftEdgeDirs.last)
-            }
-
-        if (enableDebugPrint)
-          (p1.x until p2.x)
-            .flatMap(x => (p1.y until p2.y).map(y => Point(x, y)))
-            .foreach(p => tiles.addOne(p, if (isIncluded) 1 else 0))
-
-        if (isIncluded) math.abs((p2.x - p1.x) * (p2.y - p1.y))
-        else 0
-      })
+      .filter { case (Point(x, y), _) =>
+        // find rectangles where topLeft either:
+        // - is on vertical edge
+        // - is on horizontal edge
+        // - the first and last vertical edge to the left has the same direction
+        lazy val leftEdgeDirs = verticalEdges
+          .takeWhile((a, _, _) => a.x < x)
+          .filter((a, b, d) => (a.y to b.y by d.y).contains(y))
+          .map((_, _, dir) => dir)
+        horizontalEdges.exists((a, b, d) => y == a.y && (a.x to b.x by d.x).contains(x)) ||
+        verticalEdges.exists((a, b, d) => x == a.x && (a.y to b.y by d.y).contains(y)) ||
+        (leftEdgeDirs.length > 0 && leftEdgeDirs.head == leftEdgeDirs.last)
+      }
+      .map((p1, p2) => math.abs((p2.x - p1.x) * (p2.y - p1.y)))
       .sum
-    if (enableDebugPrint)
-      debugPrint(tiles, bottomRight + Point(1, 1))
-      (Seq(points.last) ++ points.take(points.length - 1))
-        .zip(commands)
-        .filter((p, c) => p.y == 0)
-        .foreach((p, c) => println(s"$p: $c"))
-
-    area
-
-  def debugPrint(
-      tiles: mut.Map[Point, Long],
-      size: Point
-  ): Unit =
-    if (!enableDebugPrint) return
-    val Point(width, height) = size
-    for (y <- 0 until height.toInt)
-      val line = (0 until width.toInt).map: x =>
-        val p = Point(x, y)
-        if (tiles.contains(p)) tiles(p).toString.reverse.padTo(1, ' ').reverse
-        else "."
-      println(line.mkString)
-    println("")
-  end debugPrint
 
   def parseCommands(input: String) =
     val lineRegex = """(\S) (-?\d+) \(#(.+)\)""".r
