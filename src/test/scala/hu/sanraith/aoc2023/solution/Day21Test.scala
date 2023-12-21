86824edc017940ca4c430d65afa35257a0b1ec24
Soma Zsják <sanraith@users.noreply.github.com>
2023-12-21 13:18:58 +0000

    Working on day 21 part 2.

--- src/test/scala/hu/sanraith/aoc2023/solution/Day21Test.scala
+++ src/test/scala/hu/sanraith/aoc2023/solution/Day21Test.scala
@@ -7,12 +7,61 @@ class Day21Test extends SolutionTestSpec:
   describe("Day21 for example input") {
     given day: Solution = Day21()
 
-    it("solves part 1"):
-      day.asInstanceOf[Day21].part1MaxSteps = 6
-      assertPart(
-        day.part1,
-        expected = 16,
-        input = """
+    // it("solves part 1 for 6 steps"):
+    //   day.asInstanceOf[Day21].part1MaxSteps = 6
+    //   assertPart(day.part1, expected = 16, input = example)
+
+    // it("solves part 2 for 6 steps"):
+    //   day.asInstanceOf[Day21].part2MaxSteps = 6
+    //   assertPart(day.part2, expected = 16, input = example)
+    // it("solves part 2 for 10 steps"):
+    //   day.asInstanceOf[Day21].part2MaxSteps = 10
+    //   assertPart(day.part2, expected = 50, input = example)
+    // it("solves part 2 for 50 steps"):
+    //   day.asInstanceOf[Day21].part2MaxSteps = 50
+    //   assertPart(day.part2, expected = 1594, input = example)
+    // it("solves part 2 for 100 steps"):
+    //   day.asInstanceOf[Day21].part2MaxSteps = 100
+    //   assertPart(day.part2, expected = 6536, input = example)
+    // it("solves part 2 for 500 steps"):
+    //   day.asInstanceOf[Day21].part2MaxSteps = 500
+    //   assertPart(day.part2, expected = 167004, input = example)
+    // it("solves part 2 for 1000 steps"):
+    //   day.asInstanceOf[Day21].part2MaxSteps = 1000
+    //   assertPart(day.part2, expected = 668697, input = example)
+    // // it("solves part 2 for 5000 steps"):
+    // //   day.asInstanceOf[Day21].part2MaxSteps = 5000
+    // //   assertPart(day.part2, expected = 16733044, input = example)
+  }
+
+  describe("Day21 for custom input") {
+    given day: Solution = Day21()
+
+    // it("solves part 2 for 27 steps"):
+    //   day.asInstanceOf[Day21].part2MaxSteps = 27
+    //   assertPart(day.part2, expected = 588, input = part2CustomExample)
+    it("solves part 2 for 50 steps"):
+      day.asInstanceOf[Day21].part2MaxSteps = 4 * 11 + 5
+      assertPart(day.part2, expected = 1878, input = part2CustomExample)
+    // it("solves part 2 for 100 steps"):
+    //   day.asInstanceOf[Day21].part2MaxSteps = 100
+    //   assertPart(day.part2, expected = 7645, input = part2CustomExample)
+    // it("solves part 2 for 500 steps"):
+    //   day.asInstanceOf[Day21].part2MaxSteps = 500
+    //   assertPart(day.part2, expected = 188756, input = part2CustomExample)
+    // it("solves part 2 for 1000 steps"):
+    //   day.asInstanceOf[Day21].part2MaxSteps = 1000
+    //   assertPart(day.part2, expected = 753480, input = part2CustomExample)
+  }
+
+  describe("Day21 for puzzle input") {
+    given day: Solution = Day21()
+
+    // it("solves part 1")(assertPart(day.part1, 3788))
+    it("solves part 2")(_assertPart(day.part2, "__PART_2_EXPECTED__"))
+  }
+
+  val example = """
 ...........
 .....###.#.
 .###.##..#.
@@ -24,19 +73,16 @@ class Day21Test extends SolutionTestSpec:
 .##.#.####.
 .##..##.##.
 ..........."""
-      )
 
-    it("solves part 2"):
-      _assertPart(
-        day.part2,
-        expected = "__PART_2_TEST_EXPECTED__",
-        input = """__PART_2_TEST_INPUT__"""
-      )
-  }
-
-  describe("Day21 for puzzle input") {
-    given day: Solution = Day21()
-
-    it("solves part 1")(assertPart(day.part1, 3788))
-    it("solves part 2")(_assertPart(day.part2, "__PART_2_EXPECTED__"))
-  }
+  val part2CustomExample = """
+...........
+......##.#.
+.###..#..#.
+..#.#...#..
+....#.#....
+.....S.....
+.##......#.
+.......##..
+.##.#.####.
+.##...#.##.
+..........."""
